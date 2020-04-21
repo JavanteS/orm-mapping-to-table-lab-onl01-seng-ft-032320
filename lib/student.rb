5dc44c5d6e24490e1cc9a03910b9598414613971
Javante Nelson <javantenelson@gmail.com>
2020-04-21 16:59:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
   def save 
     sql = "INSERT INTO students (name, grade) values (?, ?)"
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end 
   
   def self.create(hash)
