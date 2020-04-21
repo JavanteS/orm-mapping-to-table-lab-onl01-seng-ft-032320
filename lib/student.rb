d8dbc8c313e07f07d5d04a88f874527143cfa7e4
Javante Nelson <javantenelson@gmail.com>
2020-04-21 16:46:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
       CREATE TABLE IF NOT EXISTS songs (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        grade INTEGER
+        grade NUMERIC
         )
         SQL
     DB[:conn].execute(sql)
